58b7efeb2474eb26eaea46541387831e8d77161d
Eduard Chiticari <eduard.kitikari@gmail.com>
2020-01-06 05:23:23 +0000

    Finished exercise-Generates list of domain names

--- src/js/index.js
+++ src/js/index.js
@@ -9,3 +9,22 @@ import "../style/index.scss";
 window.onload = function() {
   console.log("Hello Rigo from the console!");
 };
+window.onload = () => {
+  document.querySelector("#excuse").innerHTML = domainGenerator();
+};
+
+let domainGenerator = () => {
+  let domain = ["the", "one", "our", "my", "his"];
+  let adj = ["big", "tiny", "cool", "cozy", "warm"];
+  let root = ["tree", "bed", "room", "home", "man"];
+  let end = [".com", ".net", ".ru", ".de", ".ca"];
+  for (let i = 0; i < domain.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < root.length; k++) {
+        for (let l = 0; l < end.length; l++) {
+          document.write(domain[i] + adj[j] + root[k] + end[l] + " ");
+        }
+      }
+    }
+  }
+};
