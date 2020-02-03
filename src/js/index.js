689dd3411148bbeb0e45283144c4e54b285f2db6
Eduard Chiticari <eduard.kitikari@gmail.com>
2020-02-03 03:32:00 +0000

    changed document.write to push() and added styling to h2

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,10 @@
 /* eslint-disable */
 
 import "file-loader?name=[name].[ext]!../index.html";
-import "../assets/img/rigo-baby.jpg";
-import "../assets/img/4geeks.ico";
+
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
-};
 window.onload = () => {
   document.querySelector("#excuse").innerHTML = domainGenerator();
 };
@@ -18,13 +14,15 @@ let domainGenerator = () => {
   let adj = ["big", "tiny", "cool", "cozy", "warm"];
   let root = ["tree", "bed", "room", "home", "man"];
   let end = [".com", ".net", ".ru", ".de", ".ca"];
+  let magic = [];
+
   for (let i = 0; i < domain.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < root.length; k++) {
-        for (let l = 0; l < end.length; l++) {
-          document.write(domain[i] + adj[j] + root[k] + end[l] + " ");
-        }
+        for (let l = 0; l < end.length; l++)
+          magic.push(" " + domain[i] + adj[j] + root[k] + end[l]);
       }
     }
   }
+  return magic.toString();
 };
